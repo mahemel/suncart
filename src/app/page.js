a3a743685a420aa954bcdaa7819b7d26b3e4848f
mahemel <mahbubhasanhemel@gmail.com>
2026-05-01 23:51:42 +0600

    Summer Care tips section added on homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Banner from "@/components/homepage/Banner";
 import Products from "@/components/homepage/Products";
+import SummerCare from "@/components/homepage/SummerCare";
 
 export default function Home() {
   return (
     <>
       <Banner></Banner>
       <Products></Products>
+      <SummerCare></SummerCare>
 
     </>
   );
