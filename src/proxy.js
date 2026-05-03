0b6f730e4f309550777d2a883f8d02d43efb2940
mahemel <mahbubhasanhemel@gmail.com>
2026-05-03 06:27:09 +0600

    fixed login page issue

--- src/proxy.js
+++ src/proxy.js
@@ -1,20 +1,3 @@
-// import { NextResponse } from 'next/server'
-// import { auth } from './lib/auth'
-// import { headers } from 'next/headers'
-
-// export async function proxy(request) {
-//     const session = await auth.api.getSession({
-//         headers: await headers()
-//     })
-//     if (!session) {
-//         return NextResponse.redirect(new URL('/login', request.url))
-//     }
-// }
-
-// export const config = {
-//     matcher: ['/profile', '/update-profile', '/products/:path'],
-// }
-
 import { NextResponse } from "next/server";
 import { auth } from "./lib/auth";
 
